05fa7fa8dd456b654c7fb9164930f4f8e3797c64
winu1111 <tenshi.wankosoba88@gmail.com>
2026-06-09 19:35:54 +0900

    Update preset.js

--- grichmaker1928/js/preset.js
+++ grichmaker1928/js/preset.js
@@ -1,1 +1,50 @@
+export const presets = {
 
+    subtle: {
+
+        rgb: {
+
+            count:10,
+
+            minSize:30,
+
+            maxSize:100,
+
+            minStrength:2,
+
+            maxStrength:10
+        }
+    },
+
+    heavy: {
+
+        rgb: {
+
+            count:40,
+
+            minSize:50,
+
+            maxSize:250,
+
+            minStrength:10,
+
+            maxStrength:40
+        }
+    },
+
+    chaos: {
+
+        rgb: {
+
+            count:80,
+
+            minSize:20,
+
+            maxSize:400,
+
+            minStrength:5,
+
+            maxStrength:60
+        }
+    }
+};
