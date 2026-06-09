05e763c4c82968931b11faa1673e9230fbf4e95d
winu1111 <tenshi.wankosoba88@gmail.com>
2026-06-09 16:28:22 +0900

    Update ui.js

--- grichmaker1928/js/ui.js
+++ grichmaker1928/js/ui.js
@@ -1,1 +1,67 @@
+export function setupUI(callbacks){
 
+    const imageInput =
+        document.getElementById(
+            "imageInput"
+        );
+
+    imageInput.addEventListener(
+        "change",
+        e => {
+
+            const file =
+                e.target.files[0];
+
+            if(!file) return;
+
+            const img =
+                new Image();
+
+            img.onload = () =>
+                callbacks.onImageLoad(
+                    img
+                );
+
+            img.src =
+                URL.createObjectURL(
+                    file
+                );
+        }
+    );
+
+    document
+        .getElementById(
+            "generateBtn"
+        )
+        .addEventListener(
+            "click",
+            callbacks.onGenerate
+        );
+
+    document
+        .getElementById(
+            "resetBtn"
+        )
+        .addEventListener(
+            "click",
+            callbacks.onReset
+        );
+
+    document
+        .getElementById(
+            "savePNG"
+        )
+        .addEventListener(
+            "click",
+            callbacks.onSavePNG
+        );
+
+    document
+        .getElementById(
+            "saveJPEG"
+        )
+        .addEventListener(
+            "click",
+            callbacks.onSaveJPEG
+        );
+}
