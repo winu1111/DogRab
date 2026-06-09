66ee756cd3a8f7cbe0e3bb7e5c9765a54be04cba
winu1111 <tenshi.wankosoba88@gmail.com>
2026-06-09 21:04:38 +0900

    Update random.js

--- grichmaker1928/js/random.js
+++ grichmaker1928/js/random.js
@@ -1,1 +1,23 @@
+let rng = Math.random;
 
+export function setSeed(seed){
+
+    Math.seedrandom(seed);
+
+    rng = Math.random;
+}
+
+export function rand(min,max){
+
+    return (
+        rng() *
+        (max-min)
+    ) + min;
+}
+
+export function randInt(min,max){
+
+    return Math.floor(
+        rand(min,max)
+    );
+}
